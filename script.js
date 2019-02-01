aa5eed063a0be014f7c519166902f7203cd3a591
BatsGoScreechh <bird11@mctc.edu>
2019-02-01 10:08:11 -0500

    Done

---
+++ script.js
@@ -0,0 +1,6 @@
+  function myFunction() {
+    var x = document.getElementById("message").value;
+    document.getElementById("article2").innerHTML = x;
+    document.getElementById("article1").innerHTML = x;
+
+  }
